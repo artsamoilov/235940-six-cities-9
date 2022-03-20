3ea47078c3ff52ca13c669e95a0c8983859746ec
Artur Samoilov <artsamoilov@gmail.com>
2022-03-20 22:52:17 +0300

    убирает неиспользуемые переменные

--- project/src/components/tab/tab.tsx
+++ project/src/components/tab/tab.tsx
@@ -1,6 +1,6 @@
 import {Link} from 'react-router-dom';
 import {useAppDispatch, useAppSelector} from '../../hooks';
-import {changeCity, getOffers} from '../../store/action';
+import {changeCity} from '../../store/action';
 
 export default function Tab(props: {cityName: string}): JSX.Element {
   const dispatch = useAppDispatch();
@@ -10,7 +10,6 @@ export default function Tab(props: {cityName: string}): JSX.Element {
     <li className='locations__item'>
       <Link to='' className={`locations__item-link tabs__item ${props.cityName === cityName ? 'tabs__item--active' : ''}`} onClick={() => {
         dispatch(changeCity(props.cityName));
-        // dispatch(getOffers());
       }}
       >
         <span>{props.cityName}</span>
--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,9 +1,7 @@
 import {createReducer} from '@reduxjs/toolkit';
-import {changeCity, getOffers} from './action';
+import {changeCity} from './action';
 import {Offers} from '../mocks/offers';
 import {CityName} from '../const';
-
-const getCityOffers = (cityName: string) => Offers.filter(({city}) => city.name === cityName);
 
 const initialState = {
   cityName: CityName.Paris,
@@ -15,7 +13,4 @@ export const reducer = createReducer(initialState, (builder) => {
     .addCase(changeCity, (state, action) => {
       state.cityName = action.payload;
     })
-    .addCase(getOffers, (state) => {
-      state.offers = getCityOffers(state.cityName);
-    });
 });
