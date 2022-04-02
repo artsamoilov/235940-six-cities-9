5d07b6ef52a1deb68ea7f318dcb6e691d35601b7
Artur Samoilov <artsamoilov@gmail.com>
2022-04-02 22:51:48 +0300

    убирает преждевременную загрузку избранного

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {store} from './store';
-import {fetchOffersAction, checkAuthAction, fetchFavoritesAction} from './store/api-actions';
+import {fetchOffersAction, checkAuthAction} from './store/api-actions';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './components/app/app';
 
 store.dispatch(fetchOffersAction());
 store.dispatch(checkAuthAction());
-store.dispatch(fetchFavoritesAction());
 
 ReactDOM.render(
   <React.StrictMode>
