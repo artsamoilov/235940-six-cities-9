d6992f018b624abc7e033f608074f0dae46256d6
Artur Samoilov <artsamoilov@gmail.com>
2022-04-05 00:21:42 +0300

    добавляет тест компонента Header

---
+++ project/src/components/header/header.test.tsx
@@ -0,0 +1,21 @@
+import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import HistoryRouter from '../../components/history-route/history-route';
+import Header from './header';
+
+describe('Component: Header', () => {
+  it('should render correctly', () => {
+    const history = createMemoryHistory();
+
+    render(
+      <HistoryRouter history={history}>
+        <Header>
+          <p>Test</p>
+        </Header>
+      </HistoryRouter>
+    );
+
+    expect(screen.getByText(/Test/i)).toBeInTheDocument();
+  });
+});
+
