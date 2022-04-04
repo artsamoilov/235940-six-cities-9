469e446780a1927b77db288c239e1d6071aaed62
Artur Samoilov <artsamoilov@gmail.com>
2022-04-05 00:10:42 +0300

    добавляет тест компонента FavoritesEmpty

---
+++ project/src/components/favorites-empty/favorites-empty.test.tsx
@@ -0,0 +1,20 @@
+import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import HistoryRouter from '../../components/history-route/history-route';
+import FavoritesEmpty from './favorites-empty';
+
+describe('Component: FavoritesEmpty', () => {
+  it('should render correctly', () => {
+    const history = createMemoryHistory();
+
+    render(
+      <HistoryRouter history={history}>
+        <FavoritesEmpty />
+      </HistoryRouter>
+    );
+
+    expect(screen.getByText(/Nothing yet saved./i)).toBeInTheDocument();
+    expect(screen.getByText(/Save properties to narrow down search or plan your future trips./i)).toBeInTheDocument();
+  });
+});
+
