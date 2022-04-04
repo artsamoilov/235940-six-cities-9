fb5cd980b2bc6543127f149712675f92e444192c
Artur Samoilov <artsamoilov@gmail.com>
2022-04-05 00:14:45 +0300

    добавляет тест компонента Footer

---
+++ project/src/components/footer/footer.test.tsx
@@ -0,0 +1,19 @@
+import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import HistoryRouter from '../../components/history-route/history-route';
+import Footer from './footer';
+
+describe('Component: Footer', () => {
+  it('should render correctly', () => {
+    const history = createMemoryHistory();
+
+    render(
+      <HistoryRouter history={history}>
+        <Footer />
+      </HistoryRouter>
+    );
+
+    expect(screen.getByAltText(/6 cities logo/i)).toBeInTheDocument();
+  });
+});
+
