90f5bcd99fbcd43a3d90e4f4c8baefa14ccacec7
Artur Samoilov <artsamoilov@gmail.com>
2022-04-05 03:13:15 +0300

    добавляет тест компонента Tabs

--- project/src/components/tab/tab.test.tsx
+++ project/src/components/tab/tab.test.tsx
@@ -2,13 +2,12 @@ import {render, screen} from '@testing-library/react';
 import {createMemoryHistory} from 'history';
 import {configureMockStore} from '@jedmao/redux-mock-store';
 import {Provider} from 'react-redux';
-import {SortingOption} from '../../const';
-import {PARIS} from '../../mocks/cities';
+import {CityName} from '../../const';
 import HistoryRouter from '../../components/history-route/history-route';
 import Tab from './tab';
 
 const mockStore = configureMockStore();
-const store = mockStore({VIEW: {cityName: PARIS.name}});
+const store = mockStore({VIEW: {cityName: CityName.Paris}});
 
 describe('Component: Tab', () => {
   it('should render correctly', () => {
@@ -17,11 +16,11 @@ describe('Component: Tab', () => {
     render(
       <Provider store={store}>
         <HistoryRouter history={history}>
-          <Tab cityName={PARIS.name} />
+          <Tab cityName={CityName.Amsterdam} />
         </HistoryRouter>
       </Provider>
     );
 
-    expect(screen.getByText(new RegExp(`${PARIS.name}`, 'i'))).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(`${CityName.Amsterdam}`, 'i'))).toBeInTheDocument();
   });
 });
