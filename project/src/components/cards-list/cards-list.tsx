2af8e879122ce03c15e9a06522d508e87c31adae
Artur Samoilov <artsamoilov@gmail.com>
2022-04-07 05:03:47 +0300

    исправляет компонент списка карточек

--- project/src/components/cards-list/cards-list.tsx
+++ project/src/components/cards-list/cards-list.tsx
@@ -1,4 +1,4 @@
-import {memo, useCallback} from 'react';
+import {memo} from 'react';
 import {OfferType} from '../../types/offer-type';
 import {useAppSelector} from '../../hooks';
 import {sortOffers} from '../../common';
@@ -11,24 +11,16 @@ type PropsType = {
 
 function CardsList({onCardHover, offers}: PropsType): JSX.Element {
   const {cityName, sortingType} = useAppSelector(({VIEW}) => VIEW);
+
+  const onCurrentCardHover = onCardHover ? (id: number) => onCardHover(id) : undefined;
+
   const currentCityOffers = offers.filter(({city}) => city.name === cityName);
   const sortedCityOffers = sortOffers(currentCityOffers, sortingType);
-
-  if (onCardHover) {
-    const onCurrentCardHover = useCallback((id: number) => onCardHover(id), [onCardHover]);
-
-    return (
-      <>
-        {sortedCityOffers.map((offer: OfferType): JSX.Element =>
-          <Card key={offer.id} offer={offer} onCurrentCardHover={onCurrentCardHover} />)}
-      </>
-    );
-  }
 
   return (
     <>
       {sortedCityOffers.map((offer: OfferType): JSX.Element =>
-        <Card key={offer.id} offer={offer} onCurrentCardHover={undefined} />)}
+        <Card key={offer.id} offer={offer} onCurrentCardHover={onCurrentCardHover} />)}
     </>
   );
 }
